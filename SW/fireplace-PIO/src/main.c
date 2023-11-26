cb89f44e425586e3035594ca413fc434f94ec61c
Daniel Quach <38056275+Muoshy@users.noreply.github.com>
2023-11-26 23:13:17 +0100

    Added adc read example and fixed printf

--- SW/fireplace-PIO/src/main.c
+++ SW/fireplace-PIO/src/main.c
@@ -1,7 +1,6 @@
 #include "ch32v003fun.h"
 #include <stdio.h>
 
-
 #define WS2812BSIMPLE_IMPLEMENTATION
 //#define WS2812BSIMPLE_NO_IRQ_TWEAKING
 #include "ws2812b_simple_mod2.h"
@@ -9,6 +8,8 @@
 #include "frames.h"
 
 #include "buzzer.h"
+
+#include "photores.h"
 
 // Pinouts
 #define NEO_PORT GPIOD
@@ -22,6 +23,7 @@
 #define TOUCH_2_PIN 3
 #define TOUCH_3_PIN 4
 
+
 int main()
 {
 	SystemInit();
@@ -33,39 +35,28 @@ int main()
 	NEO_PORT->CFGLR &= ~(0xf<<(4*NEO_PIN));
 	NEO_PORT->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*NEO_PIN);
 
-	// GPIO D4 Push-Pull
-	GPIOD->CFGLR &= ~(0xf<<(4*4));
-	GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*4);
+	// init TIM2 peripheral 
+	//t2pwm_init();
 
-	// GPIO D6 Push-Pull
-	GPIOD->CFGLR &= ~(0xf<<(4*6));
-	GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*6);
-
-    // GPIO D5 Push-Pull
-    GPIOD->CFGLR &= ~(0xf<<(4*5));
-    GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*5);
-	 
-	t2pwm_init();
-
+	// start systick IRQ to run timer
     //systick_init();
+
+	printf("initializing adc...");
+	adc_init();
+	printf("done.\n\r");
 
 	while(1)
 	{
- 		// GPIOD->BSHR = (1<<0) | (1<<4) | (1<<6);	 // Turn on GPIOs
-		// GPIOC->BSHR = (1<<3);
-		// Delay_Ms( 250 );
-		// GPIOD->BSHR = (1<<16) | (1<<(16+4)) | (1<<(16+6)); // Turn off GPIOs
-		// GPIOC->BSHR = (1<<(16+3));
-		// Delay_Ms( 250 ); 
 
 		// WS2812BSimpleSend(GPIOC, 6, santa, 64);
 		for(uint8_t i = 0; i < 3; i++){
-			WS2812BSimpleSend(NEO_PORT, NEO_PIN, snowman[i], 64);
+			//WS2812BSimpleSend(NEO_PORT, NEO_PIN, snowman[i], 64);
+			printf( "adc: %d\n", adc_get() );
 			Delay_Ms( 200 );
+			
 		}
 		
-
-		// printf( "Hello World!\n");
+		//printf( "Hello World!\n");
 		// Delay_Ms( 1000 );
 	}
 }
